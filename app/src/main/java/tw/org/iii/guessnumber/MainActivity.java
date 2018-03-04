58f5d8552c83e84d6b7b92e0c6d4cdfd3c7315b1
Brad <brad@brad.tw>
2018-03-04 15:26:16 +0800

    v4

--- app/src/main/java/tw/org/iii/guessnumber/MainActivity.java
+++ app/src/main/java/tw/org/iii/guessnumber/MainActivity.java
@@ -2,12 +2,83 @@ package tw.org.iii.guessnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView log;
+    private EditText input;
+    private Button guess;
+    private String answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        log = findViewById(R.id.log);
+        input = findViewById(R.id.input);
+        guess = findViewById(R.id.guess);
+        guess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doGuess();
+            }
+        });
+
+        initGame();
     }
+
+    private void initGame(){
+        answer = createAnswer(3);
+        input.setText("");
+        log.setText("");
+    }
+
+    private void doGuess(){
+        String strInput = input.getText().toString();
+
+        input.setText("");
+
+        String result = checkAB(answer, strInput);
+        log.append(strInput + ":" + result + "\n");
+
+    }
+
+    static String checkAB(String a, String g) {
+        int A, B; A = B = 0;
+
+        for (int i=0 ;i<a.length(); i++) {
+            if (g.charAt(i) == a.charAt(i)) {
+                A++;
+            }else if (a.indexOf(g.charAt(i)) != -1) {
+                B++;
+            }
+        }
+
+        return A + "A" + B +"B";
+    }
+
+    static String createAnswer(int d) {
+        int[] poker = new int[10];
+        for (int i=0; i<poker.length; i++) poker[i] = i;
+
+        for (int i=poker.length; i>0; i--) {
+            int rand = (int)(Math.random()*i);
+            int temp = poker[rand];
+            poker[rand] = poker[i-1];
+            poker[i-1] = temp;
+        }
+
+        String ret = "";
+        for (int i=0; i<d; i++) {
+            ret += poker[i];
+        }
+
+        return ret;
+    }
+
 }
